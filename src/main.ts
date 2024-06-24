c9f064797cffcccd7b59b0bc0721702ef3d2cc46
Lucas SAISSI <lucas.saissi@gmail.com>
2024-06-24 13:06:24 +0200

    feat: set origin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://m2a.jaipasraisonla.team',
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  });
   await app.listen(80);
 }
 bootstrap();
